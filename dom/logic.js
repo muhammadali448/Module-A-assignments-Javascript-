f707707ca12a1e71d1ad4203f10f44d11f8ffed6
Ali448 <33634855+Ali448@users.noreply.github.com>
2018-03-12 22:57:13 +0500

    Complete 9 tasks

--- dom/logic.js
+++ dom/logic.js
@@ -55,3 +55,48 @@ function insert_Row() {
   ++row;
   col = 1;
 }
+
+// Task 6
+function changeContent() {
+  let selectTable = document.getElementById('myTable');
+  let selectRow = document.getElementById('roww').value;
+  let selectCol = document.getElementById('coll').value;
+  let updateText = document.getElementById('upText').value;
+  if (selectRow <= 3 && selectCol <= 2) {
+    selectTable.rows[selectRow - 1].cells[selectCol - 1].textContent = updateText;
+  }else {
+    alert('The row and column not exist');
+  }
+}
+
+// Task 7
+function removecolor() {
+  let colorSelect = document.getElementById('colorSelect');
+  colorSelect.remove(colorSelect.selectedIndex);
+}
+
+// Task 8
+function getOptions() {
+  let mySelect = document.getElementById('mySelect');
+  let valueArr = new Array();
+  for(var i = 0; i < mySelect.length; i++) {
+    valueArr.push(mySelect[i].value);
+  }
+  alert(`Item length: ${mySelect.length} and Items are: ${valueArr.toString()}`);
+}
+
+// Task 9
+function makeHighlight() {
+  // let selectStrongTag = document.querySelector('#paraHigh strong');
+  let selectStrongTag = document.getElementsByTagName('strong');
+  for(var i = 0; i < selectStrongTag.length; i++) {
+    selectStrongTag[i].setAttribute('style', 'color: red;');
+  }
+}
+
+function makeNormal() {
+  let selectStrongTag = document.getElementsByTagName('strong');
+  for(var i = 0; i < selectStrongTag.length; i++) {
+    selectStrongTag[i].setAttribute('style', 'color: black;');
+  }
+}
